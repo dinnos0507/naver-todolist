c1522bb83c357d3fe6641910fd1d1ac3149f8b50
dinnos0507 <dinnos0507@gmail.com>
2025-09-10 21:53:51 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,70 @@
-import './App.css'
-import hackathonGraphic from './assets/hackathon-graphic.svg'
-import naverLogo from './assets/naver-logo.svg'
+import "./App.css";
+import { useState } from "react";
+import { Check , X} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Input } from "./components/ui/input";
+import useTodoStore from "./store/todoStore";
 
-function App() {
+const Page = () => {
+  const [input, setInput] = useState("");
+  
+  const { todos, addTodo, removeTodo } = useTodoStore();
+
+  const handleAdd = () => {
+    if (input.trim() === "") return;
+    addTodo(input.trim());
+    setInput("");
+  }
   return (
-    <div className="container">
-      <div className="content">
-        <img src={naverLogo} alt="NAVER Vietnam AI Hackathon" className="logo" />
-        
-        <div className="greeting">
-          <p className="hello">Xin chào! 안녕하세요!</p>
-          <p className="subtitle">Hello World</p>
-        </div>
-      </div>
-      
-      <img className="graphic" src={hackathonGraphic} alt="" />
-    </div>
-  )
-}
+    <div className="min-h-screen bg-gradient-to-r from-[#67C090] to-[#26667F] flex items-center justify-center p-10">
+      <div className="max-w-3xl p-4 bg-[#DDF4E7] shadow-lg rounded-lg">
+        <h2 className="flex items-center text-xl font-bold mb-4 text-gray-800">
+          <Check className="text-green-400" size={30} /> Todo List
+        </h2>
 
-export default App
+        {/* Input */}
+        <div className="flex gap-2 mb-4">
+          <Input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Add your new todo..."
+            className="flex-1 px-3 py-2 border text-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#67C090]"
+          />
+          <Button
+            onClick={handleAdd} 
+            variant="secondary" 
+            size="sm"
+            className="px-4 py-2 
+            min-w-[60px] 
+           text-black rounded-lg transition"
+          >
+            Add
+          </Button>
+        </div>
+
+        {/* List */}
+        <ul className="space-y-1.5 pb-1 ">
+          {todos.map((todo, index) => (
+            <li
+              key={index}
+              className="flex justify-between items-center p-1 bg-gray-100 rounded-lg hover:border-gray-300 hover:border-1" 
+            >
+              <span className="text-black ">{todo}</span>
+              <Button
+                onClick={() => removeTodo(index)}
+                variant="default"
+                size="xs"
+                className="text-gray-500 border-1 border-gray-300 hover:text-red-700 hover:bg-rose-300 "
+              >
+                <X/>
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/store/todoStore.ts
@@ -0,0 +1,30 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+type TodoStore = {
+  todos: string[];
+  addTodo: (todo: string) => void;
+  removeTodo: (index: number) => void;
+}
+
+const useTodoStore = create<TodoStore>()(
+  persist<TodoStore>(
+    (set) => ({
+      todos: [],
+      addTodo: (todo) =>
+        set((state) => ({
+          todos: [...state.todos, todo],
+        })),
+      removeTodo: (index) =>
+        set((state) => ({
+          todos: state.todos.filter((_, i) => i !== index),
+        })),
+    }),
+    {
+      name: "todo-storage",
+    }
+  )
+)
+
+
+export default useTodoStore;
